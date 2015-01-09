f90ac38fcc2ed51c5cf6b131a07ba28c8b6c7741
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2015-01-09 12:53:23 +0000

    Clean build

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,21 @@ module.exports = function(grunt) {
       worker: 'node TypeScript/built/local/tsc.js worker.ts'
     },
 
+    copy: {
+        dts: {
+            src: 'TypeScript/built/local/typescriptServices.d.ts', 
+            dest: 'typings/typescriptServices.d.ts'
+        },
+        services: {
+            src: 'TypeScript/built/local/typescriptServices.js', 
+            dest: 'lib/typescriptServices.js'
+        },
+        tsc: {
+            src: 'TypeScript/built/local/tsc.js', 
+            dest: 'lib/tsc.js'
+        }
+    },
+
     watch: {
       ts: {
         files: ["*.ts"],
@@ -26,14 +41,15 @@ module.exports = function(grunt) {
     },
 
     clean: {
-      js: ["tstooltip.js*", "worker.js*"]
+      js: ["tstooltip.js*", "worker.js*", "typings/typescriptServices.d.ts"]
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ["exec:tsc", "exec:tooltip", "exec:worker"]);
+  grunt.registerTask('default', ["exec:tsc", "copy", "exec:tooltip", "exec:worker"]);
   grunt.registerTask('cleaner', ["clean", "exec:tsc_clean"])
 };
--- typings/typescriptServices.d.ts
+++
@@ -1,1 +0,0 @@
-../TypeScript/built/local/typescriptServices.d.ts
